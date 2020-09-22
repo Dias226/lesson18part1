6efadfd3ab6f5f7176c4486e3f9b13b0584147b8
Dias <said.ahmetov@list.ru>
2020-09-22 19:23:56 +0600

    feat: rewrote functions to the new standard ES6 and fixed some bugs with setInterval function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
 
+  //Функция добавления 0 перед числами меньше 10
+  const checkTime = value => {
+    if (value < 10) {
+      return "0" + value;
+    } else {
+      return value;
+    }
+  };
+
   //Timer
-  function counterTimer(deadline) {
+  const counterTimer = deadline => {
     const timerHours = document.querySelector("#timer-hours"),
       timerMinutes = document.querySelector("#timer-minutes"),
       timerSeconds = document.querySelector("#timer-seconds");
 
-    function getTimerRemaining() {
+    const getTimerRemaining = () => {
       const dateStop = new Date(deadline).getTime(),
         dateNow = new Date().getTime(),
         timeRemaining = (dateStop - dateNow) / 1000,
@@ -20,33 +29,25 @@ window.addEventListener("DOMContentLoaded", () => {
         minutes,
         seconds,
       };
-    }
-    //Функция добавления 0 перед числами меньше 10
-    function checkTime(value) {
-      if (value < 10) {
-        return "0" + value;
-      } else {
-        return value;
-      }
-    }
+    };
 
-    function updateClock() {
+    const updateClock = () => {
       const timer = getTimerRemaining();
 
       timerHours.textContent = checkTime(timer.hours);
       timerMinutes.textContent = checkTime(timer.minutes);
       timerSeconds.textContent = checkTime(timer.seconds);
 
-      if (timer.timeRemaining > 0) {
-        setInterval(updateClock, 1000);
-      } else if (timer.timeRemaining < 0) {
+      if (timer.timeRemaining < 0) {
+        clearInterval(timerInterval);
         timerHours.textContent = '00';
         timerMinutes.textContent = '00';
         timerSeconds.textContent = '00';
       }
-    }
+    };
+    const timerInterval = setInterval(updateClock, 1000);
     updateClock();
-  }
+  };
 
-  counterTimer("22 september 2020");
+  counterTimer("23 september 2020");
 });

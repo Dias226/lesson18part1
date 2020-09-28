fa80253fe498543816f543ef6c51838e45df555a
Dias <said.ahmetov@list.ru>
2020-09-28 20:27:14 +0600

    feat: Wrote one handler for all events inside the toggleMenu function

--- js/script.js
+++ js/script.js
@@ -53,15 +53,15 @@ window.addEventListener("DOMContentLoaded", () => {
 
   //Menu
   const toggleMenu = () => {
-    const btnMenu = document.querySelector('.menu'),
-      menu = document.querySelector('menu');
+    const menu = document.querySelector("menu"),
+      handlerMenu = () => menu.classList.toggle("active-menu");
+    document.addEventListener('click', event => {
+      let target = event.target;
 
-    const handlerMenu = () => menu.classList.toggle('active-menu');
-
-    btnMenu.addEventListener('click', handlerMenu);
-    menu.addEventListener("click", event => {
-      const target = event.target;
-      if (target.matches('a')) {
+      if (target.matches('menu li>a') || target.closest('.menu')) {
+        handlerMenu();
+        target = target.closest('.menu');
+      } else if (menu.classList.contains('active-menu')) {
         handlerMenu();
       }
     });

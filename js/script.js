9a18e103e3b3288515527516deff97b799303064
Dias <said.ahmetov@list.ru>
2020-09-29 21:55:07 +0600

    feat: moved the checkInputCalc function inside our calculator and added animation when calculating

--- js/script.js
+++ js/script.js
@@ -308,36 +308,52 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // calculator
 
-  const calc = (price = 100) => {
-    const calcBlock = document.querySelector('.calc-block'),
-      calcType = document.querySelector('.calc-type'),
+  const calc = price => {
+    const calcType = document.querySelector('.calc-type'),
+      calcBlock = document.querySelector('.calc-block'),
       calcSquare = document.querySelector('.calc-square'),
-      calcDay = document.querySelector('.calc-day'),
       calcCount = document.querySelector('.calc-count'),
-      totalValue = document.getElementById('total');
+      calcDay = document.querySelector('.calc-day'),
+      totalValue = document.getElementById('total'),
+      inputCalc = document.querySelectorAll('.calc-block>input');
 
     const countSum = () => {
-      let total = 0,
-        countValue = 1,
-        dayValue = 1;
       const typeValue = calcType.options[calcType.selectedIndex].value,
         squareValue = +calcSquare.value;
+      let total = 0,
+        dayValue = 1,
+        countValue = 1,
+        count = 0,
+        countSpeed = 0,
+        animateId;
 
       if (calcCount.value > 1) {
         countValue += (calcCount.value - 1) / 10;
       }
 
-      if (calcDay.value < 5) {
+      if (calcDay.value && calcDay.value < 5) {
         dayValue *= 2;
-      } else if (calcDay.value < 10) {
+      } else if (calcDay.value && calcDay.value < 10) {
         dayValue *= 1.5;
       }
 
       if (typeValue && squareValue) {
-        total = Math.ceil(price * typeValue * squareValue * countValue * dayValue);
+        total = Math.ceil(price * typeValue * squareValue * dayValue * countValue);
       }
 
-      totalValue.textContent = total;
+      countSpeed = String(total).length ** 4; // selection of calculation speed
+      const animatePrice = () => {
+        animateId = requestAnimationFrame(animatePrice);
+        count += countSpeed; //animation acceleration
+        if (count <= total) {
+          totalValue.textContent = count;
+        } else {
+          totalValue.textContent = total; //count when raised to a power is not equal to total, so we print total
+          cancelAnimationFrame(animateId);
+        }
+      };
+      animateId = requestAnimationFrame(animatePrice);
+
     };
 
     calcBlock.addEventListener('change', event => {
@@ -347,9 +363,15 @@ window.addEventListener("DOMContentLoaded", () => {
         countSum();
       }
     });
+
+    inputCalc.forEach(item => {
+      item.addEventListener('input', () => {
+        item.value = item.value.replace(/\D/g, '');
+      });
+    });
   };
 
-  calc(1000);
+  calc(100);
 
 
 });
